86e41bb9f9997d2dd885de3685dae00ca13edf9e
Michael O'Loan <supercool567@hotmail.com>
2023-11-25 12:21:30 +1100

    turtle moves up, can quit game with a space key

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+#Listen for keypresses
+screen.onkeypress(player.move, 'Up')
+
+screen.onkeypress(player.quit_game, 'space')
+
+
+screen.listen()
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    print(player.dummy_variable)
+    if player.dummy_variable == False:
+        game_is_on = False
