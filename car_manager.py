fb48c5a0de4cdc6492cb874baebdefdfb8348a41
Michael O'Loan <moloan89@gmail.com>
2023-11-25 11:27:23 +1100

    The starter files from the tutorial

---
+++ car_manager.py
@@ -0,0 +1,8 @@
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+#move increment is the amout move should incrase on a new level
+
+
+class CarManager:
+    pass
